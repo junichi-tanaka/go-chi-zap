5cf2519c3afc5f821c9b02571aa160e0126cb666
Junichi TANAKA <junsan45@mercari.com>
2019-12-22 15:26:13 +0900

    Add `Recoverer` middleware

--- main.go
+++ main.go
@@ -11,8 +11,12 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))
+	})
+	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("oops")
 	})
 	http.ListenAndServe(":3333", r)
 }
